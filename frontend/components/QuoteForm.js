b0d14961db0b85d57b61c44d69af304ba877f7cf
Natalie Thompson <nataliethompson2314@gmail.com>
2024-04-08 13:24:02 -0700

    finished

--- frontend/components/QuoteForm.js
+++ frontend/components/QuoteForm.js
@@ -1,24 +1,48 @@
-import React from 'react' // 👈 you'll need the reducer hook
+import React, { useReducer } from 'react' // 👈 you'll need the reducer hook
 
 // 👇 these are the types of actions that can change state
 const CHANGE_INPUT = 'CHANGE_INPUT'
 const RESET_FORM = 'RESET_FORM'
 
 // 👇 create your initial state object here
+const initialState = {
+  authorName: '',
+  quoteText: '',
+}
 
 // 👇 create your reducer function here
+const reducer = (state, action) => {
+  switch(action.type) {
+    case 'CHANGE_INPUT': {
+      const { name, value } = action.payload
+      return { ...state, [name]: value }
+    }
+    case 'RESET_FORM': {
+      return { authorName: '', quoteText: '' }
+    }
+    default: {
+      return state
+    }
+    
+  }
+}
 
-export default function TodoForm({ createQuote = () => { } }) {
+export default function TodoForm({ createQuote }) {
   // 👇 use the reducer hook to spin up state and dispatch
-
-  const onChange = () => {
+  const [state, dispatch] = useReducer(reducer, initialState)
+  const onChange = ({ target: { name, value }}) => {
     // 👇 implement
+    dispatch({ type: CHANGE_INPUT, payload: { name, value }})
   }
   const resetForm = () => {
     // 👇 implement
+    dispatch({ type: RESET_FORM })
   }
-  const onNewQuote = () => {
+  const onNewQuote = (evt) => {
     // 👇 implement
+    evt.preventDefault()
+    const { authorName, quoteText } = state
+    createQuote({ authorName, quoteText })
     resetForm()
   }
 
@@ -28,6 +52,7 @@ export default function TodoForm({ createQuote = () => { } }) {
       <h3>New Quote Form</h3>
       <label><span>Author:</span>
         <input
+          value={state.authorName}
           type='text'
           name='authorName'
           placeholder='type author name'
@@ -40,6 +65,7 @@ export default function TodoForm({ createQuote = () => { } }) {
           name='quoteText'
           placeholder='type quote'
           onChange={onChange}
+          value={state.quoteText}
         />
       </label>
       <label><span>Create quote:</span>
